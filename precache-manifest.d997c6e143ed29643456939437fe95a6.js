46dd4f671c7aceda9259152c19568e7ebf02d9fe
rafsuntaskin <rafsun.aiub@gmail.com>
2019-08-18 03:20:58 +0600

    Updates

---
+++ precache-manifest.d997c6e143ed29643456939437fe95a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c4c7a0409d469a74355907fc836898",
+    "url": "/home-rent-calc/index.html"
+  },
+  {
+    "revision": "47b14982b452b45cdb7a",
+    "url": "/home-rent-calc/static/css/main.9093acdf.chunk.css"
+  },
+  {
+    "revision": "3716352fb34d4536ab76",
+    "url": "/home-rent-calc/static/js/2.daf3ab0f.chunk.js"
+  },
+  {
+    "revision": "47b14982b452b45cdb7a",
+    "url": "/home-rent-calc/static/js/main.987898e7.chunk.js"
+  },
+  {
+    "revision": "3daecbe98dcf1553d6bc",
+    "url": "/home-rent-calc/static/js/runtime~main.aa296b13.js"
+  }
+]);
