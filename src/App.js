44106e4ba158e08a18625be6791043d5a21c8ec4
rafsuntaskin <rafsun.aiub@gmail.com>
2019-08-18 03:18:52 +0600

    done with primary build

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Button , AppBar, Container } from '@material-ui/core';
+import TotalCard from './components/TotalCard';
+import Bills from './components/Bills'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            water: 0,
+            gas: true,
+            electricity:0,
+            selectedBase: true,
+        };
+    }
+
+    handleChange = (e) => {
+        // If you are using babel, you can use ES 6 dictionary syntax
+        // let change = { [e.target.name] = e.target.value }
+        let change = {};
+        change[e.target.name] = parseFloat(e.target.value);
+        this.setState(change)
+    }
+
+    toggleBase = (item, e) => {
+        let newState = {};
+        newState[item] = !this.state[item];
+        this.setState(newState);
+    }
+
+    updateData = (item, e) => {
+        let newState = {};
+        newState[item] = parseFloat(e.target.value);
+        this.setState(newState);
+    }
+
+    render(){
+        return (
+            <div className="App">
+                <AppBar position="relative">
+                    <h3>Rent Calc</h3>
+                </AppBar>
+                <Container maxWidth="sm">
+                    <TotalCard items={this.state}/>
+                    <Bills items={this.state} handleChange={this.updateData} toggleBase={this.toggleBase}/>
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default App;
